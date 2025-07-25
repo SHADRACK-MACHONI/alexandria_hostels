397d08de8a83075e351ec39c0dc292b76cbc890b
SHADRACK MACHONI <shadrackmachoni26@gmail.com>
2025-07-25 18:52:36 +0300

    All

--- alexandria_hostels/settings.py
+++ alexandria_hostels/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,8 +133,6 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
-import os
-import dj_database_url
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
@@ -146,8 +146,12 @@ DATABASES = {
         default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
         conn_max_age=600
     )
+
 }
 
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
 
 
